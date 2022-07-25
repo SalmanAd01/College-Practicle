12f4c9cace1f69233025fe8416ba6171e2a00ec1
SalmanAd01 <salmanadhikari415@gmail.com>
2022-07-25 10:16:56 +0530

    Added The Ping

--- main.go
+++ main.go
@@ -12,5 +12,8 @@ func main() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello")
 	}).Methods("GET")
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "pong")
+	}).Methods("GET")
 	http.ListenAndServe(":8080", router)
 }
